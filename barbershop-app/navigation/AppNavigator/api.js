b0198095c3eae820408947f8a03b9ebafa91543e
Eryc Jacinto <erycmj@gmail.com>
2025-06-03 19:57:38 -0300

    ip dinamico

--- barbershop-app/navigation/AppNavigator/api.js
+++ barbershop-app/navigation/AppNavigator/api.js
@@ -1,7 +1,15 @@
+import Constants from 'expo-constants';
 import axios from 'axios';
 
+const getBaseUrl = () => {
+  const debuggerHost = Constants.expoConfig?.hostUri || 'localhost:8081';
+  const ip = debuggerHost.split(':')[0];
+  console.log(`Using API base URL: http://${ip}:5000/api`);
+  return `http://${ip}:5000/api`;
+};
+
 const api = axios.create({
-  baseURL: 'http://10.32.85.57:5000/api', // IP local do seu PC
+  baseURL: getBaseUrl(),
 });
 
 export default api;
